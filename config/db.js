b66cc88ef00d702d5718ce57f82596bf6ccda5d0
sujan <budhasujan3@gmail.com>
2025-01-03 14:46:01 +0545

    creating database file

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
+// Function to connect to the database
 function connectDB() {
   mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("Connected to the database");
   });
 }
 
+// Export the function to be used in the app.js file
 module.exports = connectDB;
